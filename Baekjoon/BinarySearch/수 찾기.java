9fefb99e4c6bc4488dcf701b4a83195e96ab8b4e
LeeYuri <leeyr426@gmail.com>
2021-10-29 15:49:13 +0900

    [first] 수 찾기
    1. 해당 문제는 입력으로 들어온 숫자 배열이 한 번 들어오고 그 배열 안에 값이 있는지를 찾는 문제이다.
    
    2. 배열을 두 번 만들어야 하기 때문에 숫자 배열을 만들어주는 메서드를 하나 만들었다.
    
    3. 이분 탐색 메서드를 하나 만들었다. 이 메서드는 오름차순으로 정렬된 배열과 숫자 한 개를 인자로 받으며 배열 안에 숫자가 존재하는지를 T/F로 반환 해주는 메서드이다.
    
    <이분탐색>
    1. 배열의 젤 처음 위치를 left로, 마지막 위치를 right로 저장한다.
    2. left와 right의 중간 위치를 mid로 지정하고 찾으려는 값(target)과 mid위치의 값과 비교한다.
    3. mid가 target보다 작은 값을 가지면 right를 mid-1 번째 자리로 옮긴다.
    4. mid가 target보다 큰 값을 가지면 left를 mid+1 번째 자리로 옮긴다.
    5. 이 과정을 반복하고 mid와 target이 같은 값을 가지게 되면 탐색을 종료한다.
    
    전체를 탐색하는 경우 시간 복잡도가 O(n)이지만, 이분 탐색의 시간 복잡도는 O(log(n))으로 줄어든다.

---
+++ Baekjoon/BinarySearch/수 찾기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+	
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	private static int[] makeArray() throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N]; 
+		String[] str = br.readLine().split(" ");
+		for(int i=0; i<N; i++) {
+			arr[i]  = Integer.parseInt(str[i]); 
+		}
+		return arr; 
+	}
+	
+	private static boolean binarySearch(int[] arr, int n) {
+		int left = 0, mid = 0 , right = arr.length-1; 
+		while(left <= right) {
+			mid = (left+right)/2; 
+			if(arr[mid] == n) return true; 
+			if(arr[mid] < n) left = mid+1;
+			else if(arr[mid] > n) right = mid-1; 
+		}
+		return false; 
+	}
+
+	public static void main(String[] args) throws IOException{ 
+		int[] arr = makeArray(); 
+		Arrays.sort(arr);
+		int [] question = makeArray(); 
+		
+		for(int x: question) {
+			if(binarySearch(arr,x)) {
+				bw.write(1+"\n");
+				continue;
+			}
+			bw.write(0+"\n");
+		}
+		bw.flush();
+    }
+}
