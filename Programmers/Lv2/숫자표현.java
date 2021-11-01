d8a5e3fbb398727a7a8c1b96d721b18598ce84f1
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-01 09:40:36 +0900

    [first]숫자표현
    1. 1부터 입력 값 n까지 dfs로 방문을 하여 숫자의 합이 n과 같아지는 경우의 개수를 센다.
    
    2. 연속된 숫자로 표현해야 하기 때문에 dfs 함수에 1씩 증가시켜 방문한다.

---
+++ Programmers/Lv2/숫자표현.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    private int dfs(int cur, int n, int sum){
+        if(sum>n) return 0;
+        if(sum==n) return 1; 
+        return dfs(cur+1, n, sum+cur);
+    }
+    
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<=n; i++) answer += dfs(i,n,0);
+        return answer;
+    }
+}
