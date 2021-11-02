fa7a0b3cc466f71fcba2dc870139fc88f8ff3745
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-02 17:12:35 +0900

    [first] 동전0
    문제: https://www.acmicpc.net/problem/11047
    1. 사용할 수 있는 동전은 오름차순으로 입력값으로 주어지기 때문에 가장 마지막 번째 배열부터 확인한다.
    2. 사용할 수 있는 동전의 가치가 만들고자 하는 가치 K 보다 작을 경우 동전의 가치 Ai로 나눠준 몫을 동전의 개수에 카운트 한다.
    3. Ai로 나눈 나머지를 다시 Ai-1번째 동전으로 나눠주는 과정을 반복한다.
    4. 만들고자 하는 동전 가치 K가 0이 되면 동전의 개수를 출력한다.

---
+++ Baekjoon/GreedyAlgorithm/동전0.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*; 
+public class Main {
+
+	public static void main(String[] args) throws IOException{ 
+		Scanner sc = new Scanner(System.in); 
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		
+		int cnt =0; 
+		String[] str = br.readLine().split(" "); 
+		int N = Integer.parseInt(str[0]);
+		int K = Integer.parseInt(str[1]);
+		int[] arr = new int[N]; 
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		while(K!=0) {
+			if( K >= arr[N-1] ) {
+				cnt+= K / arr[N-1];
+				K %= arr[N-1];
+			}
+			N--;
+		}
+		
+		bw.write(String.valueOf(cnt));
+		bw.flush();
+    }
+}
