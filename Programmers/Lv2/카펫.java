439d5c6f5595c21cf051c39e4624b0ce5994fec7
LeeYuri <leeyr426@gmail.com>
2021-11-10 16:22:45 +0900

    [first] 카펫
    1. 전체 카펫의 개수를 센다.
    2. 전체 카펫의 약수들을 구한다.
    3. 구한 약수에서 -2를 뺀 두 수의 곱이 노란 카펫의 개수와 같으면 반환한다. (갈색 카펫을 한 줄 깔았기 때문에 위아래, 양옆의 한 줄씩 제거해야 노란 카펫의 사이즈와 같게 되므로 2씩 빼준다. )

---
+++ Programmers/Lv2/카펫.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int all = brown + yellow; 
+        int x=0, y=0; 
+        for(int i=1; i<=all/2; i++){
+            for(int j=1; j<=all/2; j++){
+                if(i*j == all && (i-2)*(j-2) == yellow) {
+                    answer[0] = j;
+                    answer[1] = i; 
+                    return answer; 
+                }
+            }
+        }
+        return answer;
+    }
+}
