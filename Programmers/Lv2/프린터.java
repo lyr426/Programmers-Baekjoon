90a15ec0e3f7f55fc071a13225d78d3336cc9c14
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-16 19:00:01 +0900

    [first] 프린터
    1. 큐에 담을 클래스 구조를 선언
    2. 프린터로 출력할 순서를 정할 큐와 주어진 문서의 우선순위 중에서 가장 큰 값을 저장해 놓을 우선순위 큐 선언
    3. 우선순위 큐가 비어있을 때까지 반복 => 모든 문서가 출력될 때까지
    4. 현재 대기중인 문서중에서 가장 큰 값을 max 변수에 저장
    5. 대기중인 문서중 가장 앞의 문서가 가장 큰 우선순위를 가질 때까지 큐의 값을 poll, add 반복
    6. 가장 우선순위가 큰 문서가 오면 출력 후 큐에서 제거

---
+++ Programmers/Lv2/프린터.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Pair{
+    int pri, loc;
+    Pair(int pri, int loc){
+        this.pri = pri;
+        this.loc = loc;
+    }
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        
+        int answer = 1;
+        Queue<Pair> que = new LinkedList<>();
+        PriorityQueue<Integer> priQ = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i<priorities.length; i++){
+            que.add(new Pair(priorities[i], i));
+            priQ.add(priorities[i]);
+        }
+
+        while(!priQ.isEmpty()){
+            int max = priQ.poll();
+            while (que.peek().pri < max) {
+                Pair p = que.poll();
+                que.add(p);
+            }
+            if(que.peek().loc == location) return answer;
+            que.poll();
+            answer++; 
+        }
+
+        return answer;
+    }
+}
