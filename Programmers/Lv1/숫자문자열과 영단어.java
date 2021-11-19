a07d595dabb81390d3e8ab16eef60e801b6b9ab5
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-19 14:20:08 +0900

    [third] 숫자 문자열과 영단어
    1. replaceAll 함수를 이용하여 간단하게 문제 해결

--- Programmers/Lv1/숫자문자열과 영단어.java
+++ Programmers/Lv1/숫자문자열과 영단어.java
@@ -1,32 +1,12 @@
 import java.util.*;
-import java.util.stream.IntStream;
-
-public class Solution {
-
-    public static void main(String args[]) {
-        String s = "one4seveneight";
-        StringBuilder sb = new StringBuilder();
+class Solution {
+    public int solution(String s) {
         String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        HashMap<String, Integer> num = new HashMap<>();
-        for(int i=0; i<10; i++) num.put(number[i], i);
 
-        int[] lengthOfNumber = {3,4,5};
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)>='a'&&s.charAt(i)<='z'){
-                for(int j=0; j<3; j++){
-                    if(num.containsKey(s.substring(i,i+lengthOfNumber[j]))){
-                        sb.append(num.get(s.substring(i,i+lengthOfNumber[j])));
-                        i = i + lengthOfNumber[j] - 1;
-                        break;
-                    }
-                }
-                continue;
-            }
-            sb.append(s.charAt(i));
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(number[i], String.valueOf(i));
         }
-        System.out.println(Integer.parseInt(sb.toString()));
-
-
+        
+        return Integer.parseInt(String.valueOf(s)); 
     }
 }
-
