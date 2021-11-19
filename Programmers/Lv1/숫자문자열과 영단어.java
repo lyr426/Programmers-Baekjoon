ecf911e62b0ba2fc38b1d85f2b643e9308d7fca3
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-19 14:16:08 +0900

    [second] 숫자문자열과 영단어
    1. for 문을 이용해 중복되는 코드 통일시킴

--- Programmers/Lv1/숫자문자열과 영단어.java
+++ Programmers/Lv1/숫자문자열과 영단어.java
@@ -1,32 +1,32 @@
 import java.util.*;
-class Solution {
-    public int solution(String s) {
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static void main(String args[]) {
+        String s = "one4seveneight";
         StringBuilder sb = new StringBuilder();
         String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         HashMap<String, Integer> num = new HashMap<>();
         for(int i=0; i<10; i++) num.put(number[i], i);
 
+        int[] lengthOfNumber = {3,4,5};
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)>='a'&&s.charAt(i)<='z'){
-                if(num.containsKey(s.substring(i,i+3))){
-                    sb.append(num.get(s.substring(i,i+3)));
-                    i+=2;
-                    continue;
-                }
-                if(num.containsKey(s.substring(i,i+4))){
-                    sb.append(num.get(s.substring(i,i+4)));
-                    i+=3;
-                    continue;
-                }
-                if(num.containsKey(s.substring(i,i+5))){
-                    sb.append(num.get(s.substring(i,i+5)));
-                    i+=4;
-                    continue;
+                for(int j=0; j<3; j++){
+                    if(num.containsKey(s.substring(i,i+lengthOfNumber[j]))){
+                        sb.append(num.get(s.substring(i,i+lengthOfNumber[j])));
+                        i = i + lengthOfNumber[j] - 1;
+                        break;
+                    }
                 }
                 continue;
             }
             sb.append(s.charAt(i));
         }
-        return Integer.parseInt(sb.toString()); 
+        System.out.println(Integer.parseInt(sb.toString()));
+
+
     }
 }
+
