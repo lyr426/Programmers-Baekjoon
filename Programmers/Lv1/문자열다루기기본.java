e0b675392581713567e559e090818924062aab4a
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-20 15:12:56 +0900

    [first] 문자열 다루기 기본
    1 . 길이가 4나 6이 아닌 경우 false 반환
    2. 문자열을 정수형으로 바꿨을 때 NumberFormatException이 발생하면 false 반환

---
+++ Programmers/Lv1/문자열다루기기본.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6) return false;  
+        try{
+            int a = Integer.parseInt(s); 
+        }
+        catch(NumberFormatException e){
+            return false; 
+        }
+        
+        return answer;
+    }
+}
