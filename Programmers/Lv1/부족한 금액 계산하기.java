12beeb2da4f93327d53341184f4c36784323824c
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-20 15:02:02 +0900

    [first] 부족한 금액 계산하기
    1. 카운트 만큼 현재 가지고 있는 금액에서 빼기
    2. answer가 0보다 크면 돈이 부족하지 않기 때문에 0을 반환
    3. 그 외의 경우는 -1을 곱해준 뒤 반환

---
+++ Programmers/Lv1/부족한 금액 계산하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = money; 
+        for(int i=0; i<count; i++){
+            answer -= (i+1) * price; 
+        }
+        if(answer >= 0) return 0;
+        return answer*-1;
+    }
+}
