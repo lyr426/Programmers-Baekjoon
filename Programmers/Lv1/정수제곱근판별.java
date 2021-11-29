74e3543fab05142aac44d5e8146d9b5ddd67e870
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-11-29 21:17:59 +0900

    [first] 정수 제곱근 판별
    1. sqrt 함수를 이용하여 제곱근 찾기
    2. 제곱근이 아닐 경우 -1 리턴

---
+++ Programmers/Lv1/정수제곱근판별.java
@@ -0,0 +1,8 @@
+import java.util.*; 
+class Solution {
+    public long solution(long n) {
+        long result = (long)Math.sqrt(n); 
+        if(result*result != n ) return -1;
+        return (result+1)*(result+1);
+    }
+}
