90d83be31ba968603fccb834b56f77296c97ddcc
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-12-15 22:27:01 +0900

    [first] 등굣길
    1. nxm 메모이제이션 배열을 선언한다.
    2. 집이 있는 위치 d[0][0]은 1로, 웅덩이가 있는 위치는 -1로 지정해준다.
    3. 메모이제이션 배열을 채워주는데, 집과 웅덩이가 나오면 continue 해주고, 그외의 곳은  왼쪽과 위쪽의 경우의 수를 더하여 값을 채워준다.

---
+++ Programmers/Lv3/등굣길.java
@@ -0,0 +1,20 @@
+class Solution {
+    final int MOD = 1_000_000_007;
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] d = new int[n][m];
+        d[0][0] = 1;
+        for(int[] x: puddles){
+            d[x[1]-1][x[0]-1] = -1;
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0) continue;
+                if(d[i][j] == -1) continue;
+                if(i-1 >= 0 && d[i-1][j] != -1) d[i][j] += d[i-1][j];
+                if(j-1 >= 0 && d[i][j-1] != -1) d[i][j] += d[i][j-1];
+                d[i][j] %= MOD;
+            }
+        }
+        return d[n-1][m-1]%MOD;
+    }
+}
