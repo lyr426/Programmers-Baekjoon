957b9e00062da260f8da4a9f9bd08c54830840af
LeeYuri <56033943+lyr426@users.noreply.github.com>
2021-12-28 22:47:56 +0900

    [first] 야근 지수
    1. 작업량 배열을 우선순위 큐에 내림차순으로 추가한다.
    2. n시간 만큼 가장 큰 수에서 1씩 뺀다.
    3. 만약 작업량이 0보다 작아지면 바로 0을 반환
    4. 큐에 있는 값들을 모두 꺼내어 피로도를 계산한다.

---
+++ Programmers/Lv3/야근지수.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
+        for(int x: works) que.add(x);
+        for(int i=0; i<n; i++) que.add(que.poll()-1);
+        if(que.peek() <= 0) return 0; 
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            answer += x*x; 
+        }
+        return answer;
+    }
+}
