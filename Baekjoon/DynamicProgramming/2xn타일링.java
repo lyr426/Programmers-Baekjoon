b478973832c9fba47ee1cc161f408b1aa2c298ac
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-01-06 14:20:27 +0900

    [first] 2xn 타일링

---
+++ Baekjoon/DynamicProgramming/2xn타일링.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+
+    
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] cnt = new int[N+1]; 
+        cnt[0] = 1; 
+        cnt[1] = 1;
+        
+        for(int i=2; i<=N; i++){
+            cnt[i] = cnt[i-1] + cnt[i-2];
+            cnt[i] %= 10007;
+        }
+        bw.write(String.valueOf(cnt[N])); 
+        bw.flush();
+        bw.close();
+    }
+    
+}
