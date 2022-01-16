fb44a3e04f86bf8b2fba8511f3ebbfcf9533da01
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-01-16 16:12:17 +0900

    [second] 소수찾기
    <효율성 검증 통과>
    1. 소수는 자신보다 작은 소수들로 나누어지지 않으면 소수다.
    2. 소수는 자신의 루트값보다 작은 소수들로 나누어지지 많으면 소수이다.

--- Programmers/Lv1/소수찾기.java
+++ Programmers/Lv1/소수찾기.java
@@ -1,11 +1,22 @@
+import java.util.*; 
 class Solution {
-    private static boolean prime(int n){
-        for(int i=2; i<n; i++) if(n%i == 0) return false; 
-        return true; 
+    private static boolean prime(int n, ArrayList list){
+        for(int i=0; i< list.size(); i++) {
+            int num = (int)list.get(i);
+            if(Math.sqrt(n) < num) return true;
+            if(n%(num) == 0) return false;
+        }
+        return true;
     }
     public int solution(int n) {
         int answer = 0;
-        for(int i=2; i<=n; i++) if(prime(i)) answer++; 
-        return answer;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=2; i<=n; i++) {
+            if(prime(i , list)) {
+                list.add(i);
+                answer++;
+            }
+        }
+        return answer; 
     }
 }
