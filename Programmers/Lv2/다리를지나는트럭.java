27c0afabcd1359e1cb3e78ac7fde6d1ba9d2e20d
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-02-02 18:19:02 +0900

    [first] 다리를 지나는 트럭
    1. 데크를 이용하여 트럭을 한 개씩 다리를 지난다.
    2. 현재 다리에 있는 트럭 대수가 최대 올라갈 수 있는 대수와 같다면 가장 처음 다리에 올라선 트럭이 다리를 통과한다.
    3. 대기중인 트럭과 현재 다리에 있는 트럭의 무게의 합이 최대 무게보다 작다면 대기중인 트럭을 다리를 지날 수 있게 된다.
    4. 최대무게보다 무겁다면  다리를 지나고 있는 트럭들이 다리를 지나칠때까지 대기한다.

---
+++ Programmers/Lv2/다리를지나는트럭.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
+        int size = 0;
+        int cur_weight = 0;
+        int cur_length = 0;
+        while (size != truck_weights.length){
+            answer++;
+            if(cur_length == bridge_length){
+                cur_weight -= bridge.removeFirst();
+                cur_length--;
+            }
+            if(cur_weight+truck_weights[size] <= weight && cur_length < bridge_length){
+                cur_weight+=truck_weights[size];
+                cur_length++;
+                bridge.add(truck_weights[size++]);
+                continue;
+            }
+            bridge.add(0);
+            cur_length++;
+        }
+        return answer+bridge_length;
+    }
+}
