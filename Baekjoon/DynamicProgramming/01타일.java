c1cc34e049acdbb3d8b1c9577099a1fe5a29e2e6
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-02-06 14:37:33 +0900

    [first] 01타일

---
+++ Baekjoon/DynamicProgramming/01타일.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+    
+    public static long MOD = 15746; 
+    
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] d = new int[N+1];
+        for(int i=1; i<=N; i++){
+            if (i<=2){
+                d[i] = i;
+                continue; 
+            }
+            d[i] = d[i-2]+d[i-1]; 
+            d[i]%=MOD;
+        }
+        bw.write(String.valueOf(d[N]%MOD)); 
+        bw.flush();
+        bw.close();
+    }
+}
