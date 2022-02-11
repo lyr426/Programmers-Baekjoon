954cac319d25382bc143654edf2b9e14e8a5a1da
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-02-11 16:17:07 +0900

    [first] 정수삼각형
    1. 다이나믹 프로그래밍으로 문제 해결
    2. 현재 위치의 윗 줄의 왼쪽, 오른쪽을 비교하여 더 큰 숫자를 선택해 숫자를 합쳐준다.
    3. 가장 마지막 줄에서 최대값을 찾아 반환

---
+++ Programmers/Lv3/정수삼각형.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer =0;
+        int n = triangle.length;
+
+        int[][] d = new int[n][n];
+        d[0][0] = triangle[0][0];
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                if(j==0) d[i][j] = d[i-1][0]+triangle[i][0];
+                else if (i==j) d[i][j] = d[i-1][j-1] + triangle[i][j];
+                else d[i][j] = Math.max(d[i-1][j-1], d[i-1][j]) + triangle[i][j];
+            }
+        }
+        for(int i=0; i<n; i++) if(d[n-1][i]>answer) answer = d[n-1][i];
+        return answer;
+    }
+}
