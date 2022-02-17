6b300cba407cd75370cead9f0e69c6cc06934da5
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-02-17 11:46:59 +0900

    [second] 가장 긴 바이토닉 부분 수열
    코드 정리

--- Baekjoon/DynamicProgramming/가장 긴 바이토닉 부분 수열.java
+++ Baekjoon/DynamicProgramming/가장 긴 바이토닉 부분 수열.java
@@ -13,26 +13,21 @@ public class Main {
         int[] d2 = new int[N+1]; 
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for(int i=1; i<=N; i++)  arr[i] =Integer.parseInt(st.nextToken());
-        
         for(int i=1; i<=N; i++) {
         	d[i] = 1;
         	for(int j=1; j<i; j++) {
-	        	if(arr[i]>arr[j]&&d[i]<d[j]+1) {
-	        		d[i] = d[j]+1; 
-	        	}
+	        	if(arr[i]>arr[j]&&d[i]<d[j]+1) d[i] = d[j]+1; 
         	}
         }
         int res = -1; 
         for(int i=N; i>=1; i--) {
         	d2[i] = 1;
         	for(int j=i; j<=N; j++) {
-	        	if(arr[i]>arr[j]&&d2[i]<d2[j]+1) {
-	        		d2[i] = d2[j]+1; 
-	        	}
+	        	if(arr[i]>arr[j]&&d2[i]<d2[j]+1) d2[i] = d2[j]+1; 
         	}
         }
         for(int i=1; i<=N; i++) {
-        	if(d[i]+d2[i]-1 >= res) {res = d[i]+d2[i]-1 ;}
+        	if(d[i]+d2[i]-1 >= res) res = d[i]+d2[i]-1 ;
         }
         bw.write(String.valueOf(res)+"\n");
         bw.flush();
