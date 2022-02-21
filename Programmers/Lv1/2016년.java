23dc7ebe56f5923840b2c2859e68eebe6997ce86
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-02-21 21:37:38 +0900

    [first] 2016년

---
+++ Programmers/Lv1/2016년.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] str = {"THU","FRI", "SAT","SUN", "MON", "TUE", "WED"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int day = 0;
+        for(int i=0; i<a-1; i++) day += month[i];
+        day+=b;
+        return str[day%7];
+    }
+}
