3c3af43255b3b0bef1d16449ba3198cb428d3945
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-04-06 21:49:39 +0900

    [first] 듣보잡
    1. 해시맵을 선언하여 입력값을 넣어준다.
    2. M개의 값을 입력받아 해시맵에 있는 값이면 리스트에 넣어준다.
    3. 리스트 안의 값을 사전순으로 정렬 뒤 출력한다.

---
+++ Baekjoon/etc/듣보잡.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> people = new HashMap<>();
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i=0; i<N; i++) people.put(br.readLine(), i);
+        for(int i=0; i<M; i++) {
+            String name = br.readLine();
+            if(people.containsKey(name)){
+                answer.add(name);
+            }
+        }
+        Collections.sort(answer);
+        bw.write(String.valueOf(answer.size())+"\n");
+        for(String s: answer) bw.write(s+"\n");
+        bw.flush();
+    }
+}
