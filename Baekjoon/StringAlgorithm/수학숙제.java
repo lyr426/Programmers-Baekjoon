389a0027fd687ad31531747c871cf3af4468c534
lyr426 <leeyr426@gmail.com>
2022-05-06 12:59:09 +0900

    [second] 수학숙제
    1. BigInteger를 이용하여 정수의 메모리를 무한대로 증가시켜 숫자 비교

--- Baekjoon/StringAlgorithm/수학숙제.java
+++ Baekjoon/StringAlgorithm/수학숙제.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.*;
 import java.io.*;
 public class Main {
@@ -6,34 +7,21 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<BigInteger> list = new LinkedList<>();
 
         for(int i=0; i<N; i++){
             String str = br.readLine();
             for(int j=0; j<str.length(); j++){
                 String num = "";
                 while (j<str.length() &&str.charAt(j) >= '0' && str.charAt(j)<='9'){
-                    if(num.length()>0&&num.charAt(0)=='0'&&str.charAt(j)=='0'){
-                        j++;
-                        continue;
-                    }
                     num+=str.charAt(j);
                     j++;
                 }
-                if(num.length()>0) {
-                    if(num.length()>1 && num.charAt(0)=='0') num = num.substring(1);
-                    list.add(num);
-                }
+                if(num.length()>0) list.add(new BigInteger(num));
             }
         }
-        Comparator<String> c = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o1.length(), o2.length());
-            }
-        };
-        Collections.sort(list, c);
-        for(String x:list) bw.write(x+"\n");
+        Collections.sort(list);
+        for(BigInteger x:list) bw.write(String.valueOf(x)+"\n");
         bw.flush();
         bw.close();
     }
