9591e912d2a6388f7e1efbc26137ab7eeffe5477
lyr426 <leeyr426@gmail.com>
2022-05-06 13:51:33 +0900

    [first] 대표 자연수

---
+++ Baekjoon/BruteForce/대표자연수.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int represent = 0;
+        int min = -1;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            int sum = 0;
+            for(int j=0; j<N; j++) sum += Math.abs(arr[i]-arr[j]);
+            if(min == -1 || min > sum || (min == sum && represent > arr[i])){
+                min = sum;
+                represent = arr[i];
+            }
+        }
+        bw.write(String.valueOf(represent));
+        bw.flush();
+        bw.close();
+    }
+}
