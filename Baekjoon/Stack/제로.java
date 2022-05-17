1da434c8b696adf74000a14de1cf8452e27751b6
lyr426 <leeyr426@gmail.com>
2022-05-17 16:04:46 +0900

    [first] 제로

---
+++ Baekjoon/Stack/제로.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Stack<Integer> stack = new Stack<>(); 
+        
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(br.readLine()); 
+            if(num == 0){stack.pop();}
+            else{stack.push(num);}
+        }
+        long sum = 0l;
+        while(!stack.empty())sum+=stack.pop();
+
+        bw.write(String.valueOf(sum));         
+        bw.flush();
+        bw.close();
+    }
+}
