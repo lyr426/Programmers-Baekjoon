4a095f4cca5e960e7870cf0ad5b9ab7fb875e37f
lyr426 <leeyr426@gmail.com>
2022-06-28 22:05:14 +0900

    [first] 카드 구매하기

---
+++ Baekjoon/DynamicProgramming/카드구매하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+
+    
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        int[] card = new int[N+1]; 
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(st.nextToken()); 
+        }
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=i; j++){
+                card[i] = Math.max(card[i], card[i-j]+arr[j]); 
+            }
+        }
+        
+        bw.write(String.valueOf(card[N])); 
+        
+        bw.flush();
+        bw.close();
+    }
+}
