7a00af89ffaa4f8d132ccaf9d4580854b9db18ee
lyr426 <leeyr426@gmail.com>
2023-01-13 00:15:44 +0900

    Update 문제집 풀이 추가

--- Baekjoon/PriorityQueue/문제집/Main.java
+++ Baekjoon/PriorityQueue/문제집/Main.java
@@ -1,6 +1,7 @@
 package 문제집;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -16,12 +17,36 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] priority = new int[M][2];
+        ArrayList<Integer>[] graph = (ArrayList<Integer>[]) new ArrayList[N+1];
+        for(int i=0; i<=N; i++) graph[i] = new ArrayList<>();
+        int[] degree = new int[N+1];
+
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            priority[i][0] = Integer.parseInt(st.nextToken());
-            priority[i][1] = Integer.parseInt(st.nextToken());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+
+            graph[first].add(second);
+            degree[second] += 1;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i=1; i<=N; i++){
+            if(degree[i] == 0) queue.add(i);
         }
 
+        while (!queue.isEmpty()){
+            int output = queue.poll();
+            bw.write(String.valueOf(output)+ " ");
+            for(int k : graph[output]){
+                degree[k] -= 1;
+                if(degree[k] == 0){
+                    queue.add(k);
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
