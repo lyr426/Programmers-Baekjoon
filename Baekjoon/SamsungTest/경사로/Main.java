6b00ce14882d6eb31fe03e5e64ff30379b2f2536
LeeYuri <56033943+lyr426@users.noreply.github.com>
2023-04-11 16:39:54 +0900

    #1 경사로 입력

---
+++ Baekjoon/SamsungTest/경사로/Main.java
@@ -0,0 +1,33 @@
+package com.example.demo2.경사로;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][N];
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int cnt = 0;
+        for(int i=0; i<N; i++) {
+            for (int j=0; j<N; j++){
+
+            }
+
+        }
+
+
+        }
+}
