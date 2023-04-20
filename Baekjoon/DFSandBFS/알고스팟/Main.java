56bd11a15747c9519171cf507c6d8af99d610c2d
lyr426 <leeyr426@gmail.com>
2023-04-20 21:35:14 +0900

    #3 알고스팟

--- Baekjoon/DFSandBFS/알고스팟/Main.java
+++ Baekjoon/DFSandBFS/알고스팟/Main.java
@@ -1,18 +1,22 @@
 package 알고스팟;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Pair{
+class Pair implements Comparable<Pair>{
     int x;
     int y;
+    int cnt;
 
-    Pair(int x, int y){
+    Pair(int x, int y, int cnt){
         this.x = x;
         this.y = y;
+        this.cnt = cnt;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        return this.cnt <= o.cnt ? -1 : 1;
     }
 }
 
@@ -23,8 +27,8 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()); // 세로
+        int N = Integer.parseInt(st.nextToken()); // 가로
         int[][] maze = new int[N][M];
         int[][] visit = new int[N][M];
 
@@ -40,23 +44,23 @@ public class Main {
         int[] dy = {0, 1, 0, -1};
         // priorityqueue 로 만들어서 pair에 부순 개수의 값을 넣어 그 값이 더 적은 큐부터 꺼내어 탐색 ㅎ !!!!
 
-        Queue<Pair> queue = new LinkedList<>();
+        PriorityQueue<Pair> queue = new PriorityQueue<>();
 
-        queue.add(new Pair(0, 0));
+        queue.add(new Pair(0, 0, 0));
         visit[0][0] = 0;
         while (!queue.isEmpty()){
             Pair pair = queue.poll();
             int curX = pair.x;
             int curY = pair.y;
 
-            for(int i=0; i<3; i++){
+            for(int i=0; i<4; i++){
                 int xn = curX + dx[i];
                 int yn = curY + dy[i];
 
-                if(xn >= 0 && xn < N && yn >= 0 && yn <M){
-                    if(visit[xn][yn] == -1 || visit[xn][yn] > maze[xn][yn] + visit[curX][curY] ){
-                        queue.add(new Pair(xn, yn));
-                        visit[xn][yn] = visit[curX][curY] + maze[xn][yn];
+                if(xn >= 0 && xn < N && yn >= 0 && yn < M){
+                    if(visit[xn][yn] == -1 ){
+                        queue.add(new Pair(xn, yn, pair.cnt + maze[xn][yn]));
+                        visit[xn][yn] = pair.cnt + maze[xn][yn];
                     }
                 }
             }
