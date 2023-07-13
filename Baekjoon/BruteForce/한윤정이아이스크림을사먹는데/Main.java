5dcb0ceef11a63616bfca54267ed24cedc28c53f
lyr426 <leeyr426@gmail.com>
2023-07-13 23:01:23 +0900

    #3 한윤정이 이탈리아에서 아이스크림을 먹는데

---
+++ Baekjoon/BruteForce/한윤정이아이스크림을사먹는데/Main.java
@@ -0,0 +1,63 @@
+package 한윤정이아이스크림을사먹는데;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int cnt = 0;
+    static int N;
+    static List<Integer>[] lists;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        lists = (ArrayList<Integer>[]) new ArrayList[N+1];
+        for(int i=0; i<=N; i++){
+            lists[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            lists[a].add(b);
+            lists[b].add(a);
+        }
+
+        for(int i=1; i<=N-2; i++){
+            List<Integer> tmpList = new ArrayList<>();
+            tmpList.add(i);
+            bruteForce(tmpList);
+        }
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bruteForce(List<Integer> tmpList) {
+        int size = tmpList.size();
+        if(size == 3) {
+            cnt += 1;
+            return;
+        }
+        int cur = tmpList.get(size-1);
+        for (int i=cur+1; i<=N; i++){
+            if(lists[cur].contains(i)) {
+                continue;
+            }
+            if(size == 2 && lists[tmpList.get(0)].contains(i)){
+                continue;
+            }
+            tmpList.add(i);
+            bruteForce(tmpList);
+            tmpList.remove(size);
+        }
+    }
+}
