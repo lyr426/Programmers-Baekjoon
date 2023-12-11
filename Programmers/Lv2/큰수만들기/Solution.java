372ef56924bb2e5bd6a2c339653d676179d1b125
lyr426 <leeyr426@gmail.com>
2023-12-11 15:12:41 +0900

    #4 큰 수 만들기

--- Programmers/Lv2/큰수만들기/Solution.java
+++ Programmers/Lv2/큰수만들기/Solution.java
@@ -3,18 +3,27 @@ package 큰수만들기;
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append(number);
-        int start = 0;
-        for(int i=0; i<k; i++){
-            int index = start;
-            if(sb.charAt(0) == '9' && sb.charAt(index+1) == '9') start++;
-            while (index<sb.length()-i-1){
-                if(sb.charAt(index) < sb.charAt(index+1)) break;
-                index++;
+        int cnt = 0;
+        int idx = 0;
+        while( cnt < k && idx <= sb.length()-2){ // cnt가 k와 같아지거나, idx 가 sb의 사이즈와 같아진다면
+            if(sb.charAt(idx)<sb.charAt(idx+1)){
+                sb.deleteCharAt(idx);
+                cnt += 1;
+                if(idx > 0 ){
+                    idx -= 1;
+                }
+                continue;
             }
-            sb.deleteCharAt(index);
+            idx += 1;
         }
+
+        if(cnt < k){
+            int remain = k - cnt;
+            sb.delete(sb.length()-remain, sb.length());
+        }
+
         return String.valueOf(sb);
     }
 }
