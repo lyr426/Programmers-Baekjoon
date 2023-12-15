c31c103a0a937e4d3185a2695cb0a30c2ea9cd65
lyr426 <leeyr426@gmail.com>
2023-12-15 19:01:03 +0900

    #4 섬연결하기

--- Programmers/Lv3/섬연결하기/Solution.java
+++ Programmers/Lv3/섬연결하기/Solution.java
@@ -1,34 +1,59 @@
-//package 섬연결하기;
-//
-//import java.lang.reflect.Array;
-//import java.util.*;
-//
-//public class Solution {
-//    public static void main(String args[]) {
-//        int len = costs.length;
-//        int answer = 0;
-//        boolean[] visit = new boolean[n];
-//        ArrayList<Integer> list = new ArrayList<>();
-//        list.add(0);
-//        for(int i=0; i<n; i++){
-//            if(!visit[i]){
-//                int min = -1;
-//                int connect = -1;
-//                for(int j=0; j<len; j++){
-//                    if(costs[j][0] == i && (min==-1||costs[j][2]<min)){
-//                        min = costs[j][2];
-//                        connect = costs[j][1];
-//                    }
-//                    if(costs[j][1] == i && (min==-1||costs[j][2]<min)){
-//                        min = costs[j][2];
-//                        connect = costs[j][0];
-//                    }
-//                }
-//                answer += min;
-//                visit[i] = true;
-//                visit[connect] = true;
-//            }
-//        }
-//        System.out.println(answer);
-//    }
-//}
+package 섬연결하기;
+
+import java.util.*;
+
+class Pair{
+    int node;
+    int value;
+
+    Pair(int node, int value){
+        this.node = node;
+        this.value = value;
+    }
+}
+class Solution {
+    public int solution(int n, int[][] costs) {
+        int len = costs.length;
+        int answer = 0;
+
+        ArrayList<Pair>[] lists = new ArrayList[n];
+        for(int i=0; i<n; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        for(int i=0; i<costs.length; i++) {
+            int from = costs[i][0];
+            int to = costs[i][1];
+            int value = costs[i][2];
+
+            lists[from].add(new Pair(to, value));
+            lists[to].add(new Pair(from, value));
+        }
+        PriorityQueue<Pair> que = new PriorityQueue<>(new Comparator<Pair>(){
+            @Override
+            public int compare(Pair o1, Pair o2){
+                return o1.value - o2.value;
+            }
+        });
+        boolean[] visit = new boolean[n];
+        for(Pair x : lists[0]){
+            que.add(x);
+        }
+        visit[0] = true;
+        while (!que.isEmpty()){
+            Pair cur = que.poll();
+            if(visit[cur.node]){
+                continue;
+            }
+            visit[cur.node] = true;
+            answer += cur.value;
+            for(Pair x : lists[cur.node]){
+                if(!visit[x.node]){
+                    que.add(x);
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
