f43857ffbe56b20ec53c3e2f74937065f943fdbf
lyr426 <leeyr426@gmail.com>
2024-02-04 11:14:31 +0900

    #1 2048(Easy) -> 코드 리팩터링(함수화)

--- Baekjoon/SamsungTest/_2048Easy/Main.java
+++ Baekjoon/SamsungTest/_2048Easy/Main.java
@@ -32,21 +32,11 @@ public class Main {
             maxNum(board);
             return;
         }
-        int[][] copyBoard = copyArray(board);
-        blockUp(copyBoard);
-        bruteforce(copyBoard, depth+1);
 
-        copyBoard = copyArray(board);
-        blockDown(copyBoard);
-        bruteforce(copyBoard, depth+1);
-
-        copyBoard = copyArray(board);
-        blockLeft(copyBoard);
-        bruteforce(copyBoard, depth+1);
-
-        copyBoard = copyArray(board);
-        blockRight(copyBoard);
-        bruteforce(copyBoard, depth+1);
+        for(int i=0; i<4; i++) {
+            int[][] movedBlock = blockMove(board, i);
+            bruteforce(movedBlock, depth+1);
+        }
     }
 
     private static void maxNum(int[][] board) {
@@ -67,118 +57,54 @@ public class Main {
         return array;
     }
 
-    private static void blockUp(int[][] board) {
-        for(int i=0; i<N; i++) {
-            Queue<Integer> que = new LinkedList<>();
-            boolean flag = false;
-            for(int j=0; j<N; j++) {
-                if(board[j][i] == 0 ) {
-                    continue;
-                }
-                if(!que.isEmpty() && que.peek() == board[j][i] && !flag){
-                    flag = true;
-                    que.add(que.remove() * 2);
-                    continue;
-                }
-                que.add(board[j][i]);
-                flag = false;
-            }
+    private static int[][] blockMove(int[][] board, int operation) {
+        int[][] copyBoard = copyArray(board);
 
-            for(int j=0; j<N; j++) {
-                if(!que.isEmpty()){
-                    board[j][i] = que.remove();
-                    continue;
+        if(operation == 0 || operation == 2) {
+            for (int i = 0; i < N; i++) {
+                Deque<Integer> que = new LinkedList<>();
+                boolean flag = false;
+                for (int j = 0; j < N; j++) {
+                    flag = blockAdd(que, operation == 0 ? board[j][i] : board[i][j], flag);
                 }
-                board[j][i] = 0;
+                for(int j=0; j<N; j++) {
+                    if(operation == 0) {
+                        copyBoard[j][i] = !que.isEmpty() ? que.removeFirst() : 0;
+                        continue;
+                    }
+                    copyBoard[i][j] = !que.isEmpty() ? que.removeFirst() : 0;
+                }
             }
         }
+        if(operation == 1 || operation == 3){
+            for (int i = 0; i < N; i++) {
+                Deque<Integer> que = new LinkedList<>();
+                boolean flag = false;
+                for(int j=N-1; j>=0; j--) {
+                    flag = blockAdd(que, operation == 1 ? board[j][i] : board[i][j], flag);
+                }
+                for(int j=N-1; j>=0; j--) {
+                    if(operation == 1) {
+                        copyBoard[j][i] = !que.isEmpty() ? que.removeFirst() : 0;
+                        continue;
+                    }
+                    copyBoard[i][j] = !que.isEmpty() ? que.removeFirst() : 0;
+                }
+            }
+        }
+
+        return copyBoard;
     }
 
-    private static void blockDown(int[][] board) {
-        for(int i=0; i<N; i++) {
-            Queue<Integer> que = new LinkedList<>();
-            boolean flag = false;
-            for(int j=N-1; j>=0; j--) {
-                if(board[j][i] == 0 ) {
-                    continue;
-                }
-                if(!que.isEmpty() && que.peek() == board[j][i] && !flag){
-                    flag = true;
-                    que.add(que.remove() * 2);
-                    continue;
-                }
-                que.add(board[j][i]);
-                flag = false;
-            }
-
-            for(int j=N-1; j>=0; j--) {
-                if(!que.isEmpty()){
-                    board[j][i] = que.remove();
-                    continue;
-                }
-                board[j][i] = 0;
-            }
+    private static boolean blockAdd(Deque<Integer> que, int value, boolean flag) {
+        if(value == 0 ) {
+            return flag;
         }
-    }
-
-    private static void blockLeft(int[][] board) {
-        for(int i=0; i<N; i++) {
-            Queue<Integer> que = new LinkedList<>();
-            boolean flag = false;
-            for(int j=0; j<N; j++) {
-                if(board[i][j] == 0 ) {
-                    continue;
-                }
-                if(!que.isEmpty() && que.peek() == board[j][i] && !flag){
-                    flag = true;
-                    que.add(que.remove() * 2);
-                    continue;
-                }
-                que.add(board[i][j]);
-                flag = false;
-            }
-
-            for(int j=0; j<N; j++) {
-                if(!que.isEmpty()){
-                    board[i][j] = que.remove();
-                    continue;
-                }
-                board[i][j] = 0;
-            }
+        if(!que.isEmpty() && que.peekLast() == value && !flag){
+            que.add(que.removeLast() * 2);
+            return true;
         }
-    }
-
-    private static void blockRight(int[][] board) {
-        for(int i=0; i<N; i++) {
-            Queue<Integer> que = new LinkedList<>();
-            boolean flag = false;
-            for(int j=N-1; j>=0; j--) {
-                if(board[i][j] == 0 ) {
-                    continue;
-                }
-                if(!que.isEmpty() && que.peek() == board[j][i] && !flag){
-                    flag = true;
-                    que.add(que.remove() * 2);
-                    continue;
-                }
-                que.add(board[i][j]);
-                flag = false;
-            }
-
-            for(int j=N-1; j>=0; j--) {
-                if(!que.isEmpty()){
-                    board[i][j] = que.remove();
-                    continue;
-                }
-                board[i][j] = 0;
-            }
-        }
-
-//        for(int i=0; i<N; i++){
-//            for(int j=0; j<N; j++){
-//                System.out.print(board[i][j]+" ");
-//            }
-//            System.out.println();
-//        }
+        que.add(value);
+        return false;
     }
 }
