73ee825ca19e47b8186892a1c7a277dc7f9fcd41
lyr426 <leeyr426@gmail.com>
2024-03-23 17:21:53 +0900

    #1 주사위 윷놀이

--- Baekjoon/SamsungTest/주사위윷놀이/Main.java
+++ Baekjoon/SamsungTest/주사위윷놀이/Main.java
@@ -20,9 +20,11 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[][] board = {{0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0},
-                         {10, 13, 16, 19, 25, 30, 35, 40, 0},
-                         {20, 22, 24, 25, 30, 35, 40, 0},
-                         {30, 28, 27, 26, 25, 30, 35, 40, 0}};
+                         {10, 13, 16, 19},
+                         {20, 22, 24},
+                         {30, 28, 27, 26},
+                         {25, 30, 35, 40, 0}};
+
         Position[] horse = new Position[4];
         for(int i=0; i<4; i++) horse[i] = new Position(0, 0);
 
@@ -45,16 +47,19 @@ public class Main {
 
         for(int i=0; i<4; i++) {
             Position cur = horse[i];
-            if(cur.y == board[cur.x].length-1){
-                continue;
-            }
             int x = cur.x;
-            int y = cur.y + move[depth] >= board[cur.x].length ? board[cur.x].length -1 : cur.y + move[depth];
-            if(board[x][y] % 10 == 0 && board[x][y] != 40 && x == 0){
+            int y = cur.y;
+            if(x > 0 && x<4 && y + move[depth] >= board[x].length) {
+                x = 4;
+                y = move[depth] - (board[cur.x].length - cur.y);
+            }else {
+                y = cur.y + move[depth] >= board[cur.x].length ? board[cur.x].length - 1 : cur.y + move[depth]; // 도착 칸을 넘어서면 도착 칸에 정지
+            }
+            if(board[x][y] % 10 == 0 && board[x][y] != 40 && x == 0){ // 10, 20, 30 칸에 멈춰선다면
                 x = board[x][y]/10;
                 y = 0;
             }
-            if(check(board, horse, i, x, y)){
+            if(check(board, horse, i, x, y)){ // 움직일 칸에 다른 말이 없다면
                 horse[i] = new Position(x, y);
                 moving(board, horse, move, score + board[x][y], depth+1);
                 horse[i] = cur;
@@ -67,11 +72,10 @@ public class Main {
         if(board[x][y] == 0) return true;
 
         for(int i=0; i<4; i++) {
-            if(i == target) continue;
-            if (horse[i].x == x && horse[i].y == y){
-                return false;
+            if(i == target) {
+                continue;
             }
-            if((board[x][y]== 25 && board[horse[i].x][horse[i].y] == 25) || (board[x][y]== 40 && board[horse[i].x][horse[i].y] == 40)){
+            if ((horse[i].x == x && horse[i].y == y) ||(board[x][y]== 40 && board[horse[i].x][horse[i].y] == 40)){
                 return false;
             }
         }
